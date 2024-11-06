a86a4557ec490f9b1b8d5fe58694ea12ede1e6a8
Maydel-c <165515798+Maydel-c@users.noreply.github.com>
2024-11-06 21:30:27 +0530

    Refactoring the read from delay buffer code to a separate function

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -59,6 +59,7 @@ private:
     int writePosition { 0 }; // tracks position on the delay buffer where data from main buffer will be copied
     
     void fillBuffer(int channel, int writePosition, float* channelData, int bufferSize, int delayBufferSize);
+    void readFromBuffer(int channel, int delayBufferSize, int bufferSize, juce::AudioBuffer<float>& buffer);
     
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DelayismAudioProcessor)
